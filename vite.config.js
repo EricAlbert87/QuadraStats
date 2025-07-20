a6997fb674e54455b4f84cf97cc68a84c2cf4c2d
Eric Albert <eric.albert8787@gmail.com>
2025-07-20 08:57:55 -0400

    Changes

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,5 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  base: '/QuadraStats/' // GitHub Pages base path
+  base: '/FirstDownAnalytics/' // GitHub Pages base path
 })
